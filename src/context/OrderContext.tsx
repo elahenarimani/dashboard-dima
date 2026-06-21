cdc19464df59954bb13bb2984cd24d59b1845396
Elahe Narimani <elahe.narimani@gmail.com>
2026-06-21 18:25:48 +0330

    add

--- src/context/OrderContext.tsx
+++ src/context/OrderContext.tsx
@@ -7,8 +7,8 @@ import {
   type SetStateAction,
 } from "react";
 
-import { data } from "@/constants/orderData";
-import type { Order } from "@/types/orderList";
+import { data } from "../constants/orderData";
+import type { Order } from "../types/orderList";
 
 interface OrderContextType {
   listData: Order[];
