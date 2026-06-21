7a6739ace05219ae561ff1fd909676162b854acc
Elahe Narimani <elahe.narimani@gmail.com>
2026-06-21 12:01:57 +0330

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { RouterProvider } from "react-router-dom";
 import "./App.css";
 import { router } from "./router";
-import ErrorBoundary from "../ErrorBoundary/index"
 
 export const App: React.FC = () => {
   return (
     <>
-      <ErrorBoundary>
-        <RouterProvider router={router} />
-      </ErrorBoundary>
+      <RouterProvider router={router} />
     </>
   );
 };
